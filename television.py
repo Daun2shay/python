355852dd2b97416e2aa1ff09094ba1a5d31349da
Cameron Groves <79292824+Daun2shay@users.noreply.github.com>
2025-11-23 14:52:10 -0600

    Update Television class with private variables
    Refactor Television class to use private attributes and type hints.

--- television.py
+++ television.py
@@ -1,53 +1,62 @@
 class Television:
-	MIN_VOLUME = 0
-	MAX_VOLUME = 100
-	MIN_CHANNEL = 1
-	MAX_CHANNEL = 999
+	# setting the maximum and minimum volume and channel variables
+	MIN_VOLUME: int = 0
+	MAX_VOLUME: int = 2
+	MIN_CHANNEL: int = 0
+	MAX_CHANNEL: int = 3
 
-	def __init__(self):
-		self.muted = False
-		self.volume = Television.MIN_VOLUME
-		self.channel = Television.MIN_CHANNEL
-		self.power_on = False
+	# setting the initial tv state
+	def __init__(self) -> None:
+		self.__muted: bool = False
+		self.__volume: int = Television.MIN_VOLUME
+		self.__channel: int = Television.MIN_CHANNEL
+		self.__power_on: bool = False
 
-	def power(self):
-		self.power_on = not self.power_on
+	# if the power is off this turns it on if it is off this turns it on
+	def power(self) -> None:
+		self.__power_on = not self.__power_on
 
-	def mute(self):
-		if self.power_on:
-			self.muted = not self.muted
+	# this mutes and unmutes the tv
+	def mute(self) -> None:
+		if self.__power_on:
+			self.__muted = not self.__muted
 
-	def channel_up(self):
-		if not self.power_on:
+	# this ups the current channel by 1 unless it is already on the max channel
+	def channel_up(self) -> None:
+		if not self.__power_on:
 			return
-		if self.channel < Television.MAX_CHANNEL:
-			self.channel += 1
+		if self.__channel < Television.MAX_CHANNEL:
+			self.__channel += 1
 		else:
-			self.channel = Television.MIN_CHANNEL
+			self.__channel = Television.MIN_CHANNEL
 
-	def channel_down(self):
-		if not self.power_on:
+	# this lowers the current channel by one unless it is already on the minimum channel
+	def channel_down(self) -> None:
+		if not self.__power_on:
 			return
-		if self.channel > Television.MIN_CHANNEL:
-			self.channel -= 1
+		if self.__channel > Television.MIN_CHANNEL:
+			self.__channel -= 1
 		else:
-			self.channel = Television.MAX_CHANNEL
+			self.__channel = Television.MAX_CHANNEL
 
-	def volume_up(self):
-		if not self.power_on:
+	# this raises the volume by one unless it is already on the max volume
+	def volume_up(self) -> None:
+		if not self.__power_on:
 			return
-		self.muted = False
-		if self.volume < Television.MAX_VOLUME:
-			self.volume += 1
+		self.__muted = False
+		if self.__volume < Television.MAX_VOLUME:
+			self.__volume += 1
 
-	def volume_down(self):
-		if not self.power_on:
+	# this lowers the volume by one unless it is already on the min volume
+	def volume_down(self) -> None:
+		if not self.__power_on:
 			return
-		if self.volume > Television.MIN_VOLUME:
-			self.volume -= 1
+		if self.__volume > Television.MIN_VOLUME:
+			self.__volume -= 1
 
-	def __str__(self):
-		if self.muted:
-			return f'Volume = {Television.MIN_VOLUME} (muted), Channel = {self.channel}'
+	# This prints the current tv state
+	def __str__(self) -> str:
+		if self.__muted:
+			return f'Volume = {Television.MIN_VOLUME} (muted), Channel = {self.__channel}'
 		else:
-			return f'Volume = {self.volume}, Channel = {self.channel}'
+			return f'Volume = {self.__volume}, Channel = {self.__channel}'
